575ec0508a75e7dd0e8abe429dd0b7a09f554f80
Rupinder Bagri <RBagri94@hotmail.com>
2020-06-22 23:43:34 -0400

    Q2.4

--- workshop/exercises-P2_tests/exercise-2-4.js
+++ workshop/exercises-P2_tests/exercise-2-4.js
@@ -12,11 +12,19 @@
 function getLetterAtIndex(str, index) {
   // Your code here
 }
+let anyString = "Test sentence";
+console.log("'" + anyString.charAt() + "'");
 
 // Add 6 more test cases
-expect(getLetterAtIndex('hello', 4), 'o');
-expect(getLetterAtIndex('goodbye', 0), 'g');
+expect(getLetterAtIndex("hello", 4), "o");
+expect(getLetterAtIndex("goodbye", 0), "g");
 
+expect(getLetterAtIndex("pen", 2), "n");
+expect(getLetterAtIndex("pencil", 4), "i");
+expect(getLetterAtIndex("marker", 3), "k");
+expect(getLetterAtIndex("phone", 6), "undefined");
+expect(getLetterAtIndex("game", 2), "m");
+expect(getLetterAtIndex("chocolate", 3), "c");
 /**
  * -------------------------------------------------------------------
  * ⚠️ No changes necessary below. ⚠️
@@ -24,7 +32,7 @@ expect(getLetterAtIndex('goodbye', 0), 'g');
  */
 function expect(result, value) {
   if (result === value) {
-    console.log('✅ Test succeeded');
+    console.log("✅ Test succeeded");
   } else {
     console.log(`⛔️ Expected “${result}” to equal “${value}”`);
   }
