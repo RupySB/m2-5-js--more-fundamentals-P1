42a4bea539f53895c75c9c5886a2df80a31de836
Rupinder Bagri <RBagri94@hotmail.com>
2020-07-12 23:45:14 -0400

    3.3 FIXED

--- workshop/exercises-P3_problems/exercise-3-3/app.js
+++ workshop/exercises-P3_problems/exercise-3-3/app.js
@@ -9,45 +9,45 @@
 const btnList = document.querySelector("#btn-list");
 
 const revertNormal = () => {
-    for (let id = 1; id < 5; id++) {
-    
-      const initButton = document.getElementById(`btn-${id}`);
-      initButton.style.opacity = '100';
-      initButton.style.backgroundColor = 'gold';
+  for (let id = 1; id < 5; id++) {
+    const mainButton = document.getElementById(`btn-${id}`);
+    mainButton.style.backgroundColor = "gold";
+    mainButton.style.opacity = "100";
+    if (id === 4) {
+      mainButton.classList.remove("jitters");
     }
-};
-
+  }
+}; //now
 revertNormal();
 
-let initButton=document.getElementById(`btn-${id}`);
-color = ["gold"];
-initButton.onClick = function(){
-    (function loop()
-    {
-        let color = color.shift();
-        initButton.backgroundColor = color;
-    }
-});
-//It keeps saying theres an error around here..im not sure what it is. 
+const toggleColor = (id, color) => {
+  const btnElement = document.getElementById(id);
+  const currentColor = btnElement.style.backgroundColor;
+
+  btnElement.style.backgroundColor = currentColor === "gold" ? color : "gold";
+};
 
 const onClick = (x) => {
-const buttonId = x.target.id;
-const buttonElement = document.getElementById(buttonId);
+  const buttonId = x.target.id;
+  const buttonElement = document.getElementById(buttonId);
   switch (buttonId) {
     case "btn-1":
-      buttonElement.style.opacity = 0;
+      buttonElement.style.opacity =
+        buttonElement.style.opacity === "100" ? "0" : "100";
       break;
     case "btn-2":
-      buttonElement.style.background = "crimson";
+      toggleColor(buttonId, "crimson");
       break;
     case "btn-3":
-      buttonElement.style.background = "lightblue";
+      toggleColor(buttonId, "lightblue");
       break;
     case "btn-4":
       buttonElement.classList.add("jitters");
       break;
     case "reset":
-    revertNormal();
+      revertNormal();
+      //NEVER FORGET TO PUT BREAKS AFTER EVERYTHING EVER AGAIN.
+      break;
     default:
       break;
   }
